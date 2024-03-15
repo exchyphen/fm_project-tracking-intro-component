c50ee6c2efe615a14b0c95fcc8047ae6229c287e
exchyphen <exc.hyphen@gmail.com>
2024-03-15 13:01:00 -0700

    complete desktop view, started on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,64 @@ import "./App.css";
 
 /* images */
 import Logo from "./assets/images/logo.svg";
+import Hero from "./assets/images/illustration-devices.svg";
 
 function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const mobileBreakpoint = 800;
+
+  /* data */
+  const data__navItems = [
+    {
+      title: "product",
+      style: "dark",
+    },
+    {
+      title: "features",
+      style: "dark",
+    },
+    {
+      title: "pricing",
+      style: "dark",
+    },
+    {
+      title: "divider",
+      style: "light",
+    },
+    {
+      title: "login",
+      style: "light",
+    },
+  ];
 
   /* item creation */
 
   /* dev: build nav item list creator -> if mobile menu, divider is a line */
+  const create__navItems = () => {
+    const mobile = mobileBreakpoint >= windowWidth;
+
+    return data__navItems.map((data, index) => {
+      return data.title === "divider" ? (
+        <div
+          key={`navItem${index}`}
+          className={
+            "nav__divider" +
+            (mobile ? " nav__divider--line" : " nav__divider--circle")
+          }
+        ></div>
+      ) : (
+        <a
+          key={`navItem${index}`}
+          className={
+            "nav__item text--other" +
+            (data.style === "light" ? " nav__item--light" : " nav__item--dark")
+          }
+        >
+          {data.title}
+        </a>
+      );
+    });
+  };
 
   /* initial load */
   useEffect(() => {
@@ -26,11 +77,30 @@ function App() {
 
       <nav className="nav">
         <img className="nav__logo" src={Logo} alt="logo"></img>
-        <div className="nav__right-container"></div>
+        <div className="nav__content-container">{create__navItems()}</div>
       </nav>
 
-      <main>
-        <article></article>
+      <main className="main">
+        <article className="main__content-container">
+          <div className="main__new-container text--other">
+            <p className="main__new">new</p>
+            <p className="main__new-feature">monograph dashboard</p>
+          </div>
+          <h1 className="text--other">powerful insights into your team</h1>
+          <p className="main__p text--body">
+            Project planning time tracking for agile teams
+          </p>
+          <div className="main__demo-container">
+            <button className="main__button text--other">
+              schedule a demo
+            </button>
+            <p className="main__preview text--other">to see a preview</p>
+          </div>
+        </article>
+
+        <div className="main__hero-wrapper">
+          <img className="main__hero" src={Hero} alt="hero img"></img>
+        </div>
       </main>
 
       <footer>
